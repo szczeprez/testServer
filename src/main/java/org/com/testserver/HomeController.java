61e6d93f5ff34d83138a131654bab2a5cd0fdc75
Piotr <szczeprez@poczta.fm>
2016-04-13 22:13:03 +0200

    nowe zmiany

--- src/main/java/org/com/testserver/HomeController.java
+++ src/main/java/org/com/testserver/HomeController.java
@@ -1,7 +1,5 @@
 package org.com.testserver;
-
-import java.text.DateFormat;
-import java.util.Date;
+ 
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,11 +7,19 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
 
 /**
  * Handles requests for the application home page.
@@ -24,31 +30,68 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
 	/**
+	 * http://websystique.com/spring-security/spring-security-4-custom-login-form-annotation-example/
+	 * 
 	 * Simply selects the home view to render by returning its name.
 	 */
+	//Spring Security see this :
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	public ModelAndView login(
+		@RequestParam(value = "error", required = false) String error,
+		@RequestParam(value = "logout", required = false) String logout) {
 
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject("error", "Invalid username and password!");
+		}
 
-		String formattedDate = dateFormat.format(date);
+		if (logout != null) {
+			model.addObject("msg", "You've been logged out successfully.");
+		}
+		model.setViewName("login");
 
-		model.addAttribute("serverTime", formattedDate);
+		return model;
 
-		return "login";
-	}
-	
+	}	
 	
 	@RequestMapping(value = "/")
-	public ModelAndView welcome(HttpServletRequest request,
-			HttpServletResponse response){
-		
-		ModelAndView model = new ModelAndView("home");
-		
-		return model; 
+	public String welcome(ModelMap model){
+	
+	    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    String name = auth.getName(); //get logged in username
+		logger.info("Logged person = " + name);
+ 
+	    model.addAttribute("username", name	); 
+		return "home"; 
 	}
+	
+	@RequestMapping(value = { "/welcome**" }, method = RequestMethod.GET)
+	public ModelAndView welcomePage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Custom Login Form");
+		model.addObject("message", "This is welcome page!");
+		model.setViewName("hello");
+		return model;
+
+	}
+	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
+	public ModelAndView adminPage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Custom Login Form");
+		model.addObject("message", "This is protected page!");
+		model.setViewName("admin");
+
+		return model;
+	}
+	
+/*    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public String adminPage(ModelMap model) {
+        model.addAttribute("user", getPrincipal());
+        return "admin";
+    }*/
+	
 	@RequestMapping(value = "menu" , method=RequestMethod.GET)
 	public String menu(Model model){
 		return "headAndFooterAndMenu/menu"; 
@@ -97,5 +140,49 @@ public class HomeController {
 		logger.info("Method kontakt() {}", locale);
 		return "pages/kontakt";
 	}
+	     
+    @RequestMapping(value = "/db", method = RequestMethod.GET)
+    public String dbaPage(ModelMap model) {
+        model.addAttribute("user", getPrincipal());
+        return "dba";
+    }
+ 
+	//for 403 access denied page
+	@RequestMapping(value = "/403", method = RequestMethod.GET)
+	public ModelAndView accesssDenied() {
 
+	  ModelAndView model = new ModelAndView();
+		
+	  //check if user is login
+	  Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	  if (!(auth instanceof AnonymousAuthenticationToken)) {
+		UserDetails userDetail = (UserDetails) auth.getPrincipal();	
+		model.addObject("username", userDetail.getUsername());
+	  }
+		
+	  model.setViewName("403");
+	  return model;
+
+	}
+ 
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){    
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/";
+    }
+    
+    private String getPrincipal(){
+        String userName = null;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+ 
+        if (principal instanceof UserDetails) {
+            userName = ((UserDetails)principal).getUsername();
+        } else {
+            userName = principal.toString();
+        }
+        return userName;
+    }
 }
--- src/main/webapp/WEB-INF/views/headAndFooterAndMenu/menu.jsp
+++ src/main/webapp/WEB-INF/views/headAndFooterAndMenu/menu.jsp
@@ -1,22 +1,47 @@
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>   
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>
        <div class="text-muted">
         <nav>
           <ul class="nav nav-pills pull-right">
-            <li role="presentation" class="active"><a href="loged/pozycjonowanie-seo">Pozycjonowanie SEO</a></li>
-            <li role="presentation"><a href="<c:url value="/loged/projektowanie-stron-www"/>">Projektowanie Stron WWW</a></li>
+            <li role="presentation" class="active"><a href="<c:url value="/loged/pozycjonowanie-seo" />">Pozycjonowanie SEO</a></li>
+            <li role="presentation"><a href="<c:url value="/loged/projektowanie-stron-www"/> ">Projektowanie Stron WWW</a></li>
             <li class="dropdown"> 
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Oferta <span class="caret"></span></a>
 	          <ul class="dropdown-menu">
-	            <li><a href="loged/projektowanie-stron-www">Projektowanie Stron WWW</a></li>
-	            <li><a href="loged/pozycjonowanie-seo">Pozycjonowanie SEO</a></li>
-	            <li><a href="sklepy-internetowe">Sklepy internetowe</a></li>
+	            <li><a href="<c:url value="/loged/projektowanie-stron-www" /> ">Projektowanie Stron WWW</a></li>
+	            <li><a href="<c:url value="/loged/pozycjonowanie-seo" /> ">Pozycjonowanie SEO</a></li>
+	            <li><a href="<c:url value="/sklepy-internetowe" /> ">Sklepy internetowe</a></li>
 	            <li role="separator" class="divider"></li>
-	            <li><a href="social-media">Social media</a></li>
+	            <li><a href="<c:url value="/social-media" /> ">Social media</a></li>
 	          </ul>
 	        </li>
-            <li role="presentation"><a href="kontakt">Kontakt</a></li>
-            <li role="presentation"><a href="users/registrationForm">Rejestracja</a></li>
-            <li role="presentation"><a href="login">Logowanie</a></li>
+            <li role="presentation"><a href='<c:url value="/kontakt"/> '>Kontakt</a></li>
+            
+            <c:if test="${pageContext.request.userPrincipal.name == null}">
+                <li role="presentation"><a href="<c:url value="/users/new"/>">Rejestracja</a></li>
+                <li role="presentation"><a href="login">Logowanie</a></li>
+            </c:if>
+            
+			<sec:authorize access="hasRole('ROLE_USER')">
+				<!-- For login user -->
+				<c:url value="/j_spring_security_logout" var="logoutUrl" />
+				<form action="${logoutUrl}" method="post" id="logoutForm">
+					<input type="hidden" name="${_csrf.parameterName}"
+						value="${_csrf.token}" />
+				</form>
+				<script>
+					function formSubmit() {
+						document.getElementById("logoutForm").submit();
+					}
+				</script>
+		
+				<c:if test="${pageContext.request.userPrincipal.name != null}">
+					<h5>
+						User : ${pageContext.request.userPrincipal.name} | <a
+							href="javascript:formSubmit()"> Logout</a>
+					</h5>
+				</c:if>
+			</sec:authorize>
           </ul>
         </nav> 
       </div>
